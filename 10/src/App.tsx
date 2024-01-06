3daf6546f0bfb5f501b6dc1c738868f03e039269
Andrew Kohn <contact@kohnandrew.com>
2024-01-06 11:13:05 -0600

    Add: components to display tiles

--- 10/src/App.tsx
+++ 10/src/App.tsx
@@ -1,58 +1,89 @@
 import { useEffect, useState } from 'react';
 
-enum Tile {
-    NorthSouth = '|',
-    EastWest = '-',
-    NorthEast = 'L',
-    NorthWest = 'J',
-    SouthWest = '7',
-    SouthEast = 'F',
-    Ground = '.',
-    Animal = 'S',
-}
+// enum Tile {
+//     NorthSouth = '|',
+//     EastWest = '-',
+//     NorthEast = 'L',
+//     NorthWest = 'J',
+//     SouthWest = '7',
+//     SouthEast = 'F',
+//     Ground = '.',
+//     Animal = 'S',
+// }
 
-enum Direction {
-    North,
-    South,
-    East,
-    West,
-}
+// enum Direction {
+//     North,
+//     South,
+//     East,
+//     West,
+// }
 
 function App() {
     const [input, setInput] = useState<string[]>([]);
+    // useEffect(() => {
+    //     fetch('../diagramSample.txt')
+    //         .then(res => res.text())
+    //         .then(data => setInput(data.split('\n')));
+    // }, []);
+
     useEffect(() => {
-        fetch('../diagramSample.txt')
+        fetch('../diagram.txt')
             .then(res => res.text())
             .then(data => setInput(data.split('\n')));
     }, []);
 
-    // TODO:
-    // have a block for each char
-    // render each tile with a padding to create the block
-    // each render matches the original
-
-    // const tiles: JSX.Element[] | undefined =
-    //     input.length > 0
-    //         ? input.map((line: string, key: number) => (
-    //   <div style={blockStyle} key={key}>
-    //       <p style={tileStyle}>{line}</p>
-    //   </div>
-    //           ))
-    //         : undefined;
-
     return (
         <main style={mainStyles}>
-            <h1>Day 10</h1>
-            {/* <div>{tiles ? tiles : ''}</div> */}
+            <h1 style={{ marginBottom: '4rem' }}>Day 10</h1>
+            <Tiles input={input} />
         </main>
     );
 }
 
-const mainStyles = {
+/////////////////////////////////////////////////////////////////
+// Tiles Component
+interface TilesProps {
+    input: string[];
+}
+
+const Tiles = ({ input }: TilesProps) => {
+    const displayTiles = (lineValues: string[]) => {
+        return lineValues.map((tile: string, key: number) => (
+            <Tile char={tile} key={key} />
+        ));
+    };
+
+    const line = Object.values(input).map((tiles: string, key: number) => (
+        <div style={lineStyle} key={key}>
+            {displayTiles(tiles.split(''))}
+        </div>
+    ));
+
+    return line ? line : null;
+};
+
+/////////////////////////////////////////////////////////////////
+// Tile Component
+interface TileProps {
+    char: string;
+}
+
+const Tile = ({ char }: TileProps) => {
+    return (
+        <div style={blockStyle}>
+            <p style={tileStyle}>{char}</p>
+        </div>
+    );
+};
+
+/////////////////////////////////////////////////////////////////
+// CSS
+const mainStyles: React.CSSProperties = {
+    marginTop: '8rem',
     backgroundColor: '#0b1215',
     color: '#f3f3f3',
     width: '100%',
-    height: '100vh',
+    height: '100%',
     display: 'flex',
     justifyContent: 'flex-start',
     alignItems: 'center',
@@ -60,11 +91,26 @@ const mainStyles = {
     flexDirection: 'column' as const,
 };
 
-const blockStyle = {
-    padding: '2rem',
-    backgroundColor: '#777',
+const lineStyle: React.CSSProperties = {
+    display: 'flex',
 };
 
-const tileStyle = {};
+const blockStyle: React.CSSProperties = {
+    width: '1.2rem',
+    height: '1.2rem',
+    boxSizing: 'border-box',
+    border: '1px solid rgba(255, 255, 0, 0.5)',
+    backgroundColor: 'rgba(255, 0, 0, 0.5)',
+    position: 'relative',
+};
+
+const tileStyle: React.CSSProperties = {
+    fontSize: '1rem',
+    fontWeight: '700',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+};
 
 export default App;
